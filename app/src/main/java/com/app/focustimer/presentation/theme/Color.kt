18cf435b7841298af048696a71c03cb247a827c2
JairGuzman1810 <jair18101@hotmail.com>
2024-12-18 10:47:25 -0600

    Added main user interface

--- app/src/main/java/com/app/focustimer/presentation/theme/Color.kt
+++ app/src/main/java/com/app/focustimer/presentation/theme/Color.kt
@@ -2,7 +2,7 @@ package com.app.focustimer.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryBlack= Color(0XFF000000)
-val primaryWhite= Color(0XFFFFFFFF)
+val primaryBlack = Color(0XFF000000)
+val primaryWhite = Color(0XFFFFFFFF)
 val gray = Color(0XFF888888)
 val lightGray = Color(0XFFCCCCCC)
